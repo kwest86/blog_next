f721f53cdc2fd00cfe23a45afbc1e313764c5bba
Kota Nishi01~ <kwest86jp@yahoo.co.jp>
2024-05-22 18:00:36 +0900

    feat テーマカラーを追加

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,51 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  fonts: {
-    heading: "var(--font-rubik)",
-    body: "var(--font-rubik)",
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+      },
+    },
+  },
+  colors: {
+    bg: {
+      primary: "#FFFFFF",
+      secondaly: "#e3f6f5",
+      tertiary: "#bae8e8",
+    },
+    headline: {
+      primary: "#272343",
+      secondary: "#2d334a",
+    },
+    button: {
+      background: "#FFB359",
+      text: "#272343",
+    },
+    highlight: "#FFB359",
+  },
+  components: {
+    Text: {
+      baseStyle: {
+        color: "headline.secondary",
+      },
+    },
+    Heading: {
+      baseStyle: {
+        color: "headline.primary",
+      },
+    },
+    Button: {
+      baseStyle: {
+        backgroundColor: "button.background",
+        color: "button.text",
+      },
+    },
+    Box: {
+      baseStyle: {
+        backgroundColor: "bg.primary",
+      },
+    },
   },
 });
 
